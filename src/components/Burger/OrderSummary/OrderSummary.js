c9edd9c2472284bc9de7654a863cb794f534e522
Alexander Yalcin <alexander.yalcin@appliedtechnology.se>
2019-06-25 21:22:45 +0200

    added nav img

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -6,13 +6,10 @@ import Button from '../../UI/Button/Button/Button';
 const orderSummary = props => {
   const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
     return (
-      <Aux>
-        <li key={igKey}>
-          <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:{' '}
-          {props.ingredients[igKey]}
-        </li>
-        
-      </Aux>
+      <li key={igKey}>
+        <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:{' '}
+        {props.ingredients[igKey]}
+      </li>
     );
   });
   return (
@@ -20,7 +17,9 @@ const orderSummary = props => {
       <h3>Your Order</h3>
       <p>Burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
-      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+      <p>
+        <strong>Total Price: {props.price.toFixed(2)}</strong>
+      </p>
       <p>Continue to Checkout?</p>
       <Button btnType="Danger" clicked={props.purschaseCancelled}>
         CANCEL
